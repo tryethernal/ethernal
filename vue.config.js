08bb2575bc5f20de5caa417c9f1fecfc8572e02b
Antoine de Chevigné <antoine@bannerman.com>
2021-01-23 17:45:15 +0100

    copy redirects file

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,17 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
     "transpileDependencies": [
         "vuetify"
     ],
     configureWebpack: {
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { from: './_redirects', to: './' }
+                ]
+            })
+        ],
         externals: {
             fsevents: "require('fsevents')",
             'fs-extra': '{}'
