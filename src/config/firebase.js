9ce22f7fd99d5d40f18b08cad3586428c316ee4a
Antoine de Chevigné <antoine@bannerman.com>
2021-01-23 17:25:56 +0100

    first commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,9 @@
 export const FIREBASE_CONFIG = {
-    apiKey: 'AIzaSyCUefO7sAM0YafO3CVslIf8Tn7eRZbXI3s',
-    authDomain: 'ethernal-95a14.firebaseapp.com',
-    databaseURL: 'https://ethernal-95a14-default-rtdb.firebaseio.com/',
-    projectId: 'ethernal-95a14',
-    storageBucket: 'ethernal-95a14.appspot.com',
-    messagingSenderId: '864612390560',
-    appId: '1:864612390560:web:b339dc0292ff15f7161bc5'
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
