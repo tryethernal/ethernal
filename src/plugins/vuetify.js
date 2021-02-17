cb64bf5c1e26e3692684a07e8004bb68e274b4b3
Antoine de Chevigné <antoine@bannerman.com>
2021-02-17 11:36:19 +0100

    update branding

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,15 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#3D95CE',
+                accent: '#29648E',
+                success: '#27E73',
+                warning: '#E78227',
+                error: '#E72732',
+            }
+        }
+    }
 });
