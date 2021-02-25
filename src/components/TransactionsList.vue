f28f68a9b6551ba2aed80818eec3e488024f1ed0
Antoine de Chevigné <antoine@bannerman.com>
2021-02-25 15:01:20 +0100

    add info in transactions list, closes #3

---
+++ src/components/TransactionsList.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-data-table
+        loading="true"
+        :items="transactions"
+        :sort-by="'blockNumber'"
+        :sort-desc="true"
+        :headers="headers"
+        item-key="hash">
+        <template v-slot:item.hash="{ item }">
+            <v-icon small v-if="!item.receipt.status" color="error lighten-1" class="mr-2">mdi-alert-circle</v-icon>
+            <Hash-Link :type="'transaction'" :hash="item.hash" />
+        </template>
+        <template v-slot:item.timestamp="{ item }">
+            {{ item.timestamp | moment('from') }}
+        </template>
+        <template v-slot:item.from="{ item }">
+            <v-chip x-small class="mr-2" v-if="item.from && item.from === currentAddress">self</v-chip>
+            <Hash-Link :type="'address'" :hash="item.from" />
+        </template>
+        <template v-slot:item.to="{ item }">
+            <v-chip x-small class="mr-2" v-if="item.to && item.to === currentAddress">self</v-chip>
+            <Hash-Link :type="'address'" :hash="item.to" />
+        </template>
+        <template v-slot:item.value="{ item }">
+            {{ item.value | fromWei }}
+        </template>
+        <template v-slot:item.fee="{ item }">
+            {{ item.gasPrice * item.gas | fromWei }}
+        </template>
+    </v-data-table>
+</template>
+
+<script>
+import FromWei from '../filters/FromWei.js';
+import HashLink from './HashLink.vue';
+
+export default {
+    name: 'TransactionsList',
+    props: ['transactions', 'currentAddress'],
+    components: {
+        HashLink
+    },
+    filters: {
+        FromWei
+    },
+    data: () => ({
+        headers: [
+            {
+                text: 'Txn Hash',
+                value: 'hash',
+                align: 'start'
+            },
+            {
+                text: 'Block',
+                value: 'blockNumber'
+            },
+            {
+                text: 'Age',
+                value: 'timestamp'
+            },
+            {
+                text: 'From',
+                value: 'from'
+            },
+            {
+                text: 'To',
+                value: 'to'
+            },
+            {
+                text: 'Value',
+                value: 'value'
+            },
+            {
+                text: 'Fee',
+                value: 'fee'
+            }
+        ]
+    })
+}
+</script>
