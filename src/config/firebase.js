85d42289b6a844b31960f5c0b7bebf3d1a99036c
Antoine de Chevigné <antoine@bannerman.com>
2021-04-08 18:49:49 +0200

    reset workspace

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,4 @@
-const _PROD_FIREBASE_CONFIG = {
+export const FIREBASE_CONFIG = {
     apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
     authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
@@ -7,11 +7,3 @@ const _PROD_FIREBASE_CONFIG = {
     messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
     appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
-
-const _DEV_FIREBASE_CONFIG = {
-    apiKey: '123',
-    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL + '?ns=ethernal-95a14-default-rtdb',
-    projectId: 'ethernal-95a14'
-};
-
-export const FIREBASE_CONFIG = process.env.NODE_ENV == 'development' ? _DEV_FIREBASE_CONFIG : _PROD_FIREBASE_CONFIG;
