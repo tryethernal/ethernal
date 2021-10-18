62c4f19841e16d7556ba8e45c1f24c6cdb26aec6
Antoine de Chevigné <antoine@bannerman.com>
2021-10-18 17:02:12 +0200

    fix crypto specs

--- functions/tests/lib/crypto.spec.js
+++ functions/tests/lib/crypto.spec.js
@@ -1,4 +1,5 @@
 const { encrypt, decrypt, encode, decode } = require('../../lib/crypto');
+const Helper = require('../helper');
 
 const ENCRYPTED_KEY = 'a12fcf9289bf85dea1b50f2c8c941e12:0b3eef87b4b4faf01bfe775ddd238900a460c9f8afd33b66f44157e90d24c7d8';
 const ENCODED_ENCRYPTED_KEY = 'eyJhbGciOiJIUzI1NiJ9.YTEyZmNmOTI4OWJmODVkZWExYjUwZjJjOGM5NDFlMTI6MGIzZWVmODdiNGI0ZmFmMDFiZmU3NzVkZGQyMzg5MDBhNDYwYzlmOGFmZDMzYjY2ZjQ0MTU3ZTkwZDI0YzdkOA.BDdEkTowTz7pCmiHg6mIGKIHRRzEELv0ZzzcSOj1eqc';
