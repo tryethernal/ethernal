310576f6dd1a9f327d16b8b3b012cd2021a677e9
Antoine <antoine@bannerman.com>
2022-03-29 11:19:09 +0200

    auto format transfer/balance amount + process transfers tokens (#140)
    * auto format transfer/balance amount + process transfers tokens
    
    * backend test update
    
    * update transfers snapshots
    
    * update snapshot

--- src/components/TokenTransfers.vue
+++ src/components/TokenTransfers.vue
@@ -10,15 +10,21 @@
             <Hash-Link :type="'address'" :hash="item.dst" :fullHash="true" :withName="true" />
         </template>
         <template v-slot:item.token="{ item }">
-            <Hash-Link :type="'address'" :hash="item.token" :withName="true" />
+            <Hash-Link :type="'address'" :hash="item.token" :withName="true" :withTokenName="true" />
         </template>
         <template v-slot:item.amount="{ item }">
-            {{ item.amount }}
+            <span v-if="decimals[item.token]">
+                {{ item.amount | fromWei('ether', symbols[item.token], decimals[item.token]) }}
+            </span>
+            <span v-else>
+                {{ item.amount }}
+            </span>
         </template>
     </v-data-table>
 </template>
 <script>
 import HashLink from './HashLink';
+import FromWei from '../filters/FromWei';
 
 export default {
     name: 'TokenTransfers',
@@ -26,13 +32,37 @@ export default {
     components: {
         HashLink
     },
+    filters: {
+        FromWei
+    },
     data: () => ({
         tableHeaders: [
             { text: 'From', value: 'src' },
             { text: 'To', value: 'dst' },
             { text: 'Token', value: 'token' },
             { text: 'Amount', value: 'amount' }
-        ]
-   })
+        ],
+        decimals: {},
+        symbols: {}
+    }),
+    mounted() {
+        for (let i = 0; i < this.transfers.length; i++) {
+            this.$set(this.symbols, this.transfers[i].token, '');
+            this.db
+                .collection('contracts')
+                .doc(this.transfers[i].token)
+                .get()
+                .then(doc => {
+                    const data = doc.data();
+
+                    if (!data) return;
+
+                    if (data.token && data.token.decimals)
+                        this.$set(this.decimals, this.transfers[i].token, data.token.decimals);
+                    if (data.token && data.token.symbol)
+                        this.$set(this.symbols, this.transfers[i].token, data.token.symbol);
+                });
+        }
+    }
 }
 </script>
--- tests/unit/components/TokenTransfers.spec.js
+++ tests/unit/components/TokenTransfers.spec.js
@@ -25,5 +25,26 @@ describe('TokenTransfers.vue', () => {
         }, 1500);
     });
 
+    it('Should display fomatted amounts if info is present on token contract', async (done) => {
+        await helper.mocks.admin
+            .collection('contracts')
+            .doc('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
+            .set({ token: { decimals: 18, symbol: 'ETL', name: 'Ethernal' }});
+
+        const wrapper = helper.mountFn(TokenTransfers, {
+            propsData: {
+                transfers: [
+                    { token: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', src: '0xC056255e6B1E6001852f9f20587A964cb89418e7', dst: '0x4f6742bADB049791CD9A37ea913f2BAC38d01279', amount: '100000000000000000' },
+                    { token: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', src: '0xC056255e6B1E6001852f9f20587A964cb89418e7', dst: '0x4f6742bADB049791CD9A37ea913f2BAC38d01279', amount: '100000000000000000' }
+                ]
+            }
+        })
+
+        setTimeout(() => {
+            expect(wrapper.html()).toMatchSnapshot();
+            done();
+        }, 1500);
+    });
+
     afterEach(() => helper.clearFirebase());
 });
