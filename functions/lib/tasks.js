aad318a0092115f977aca7f6cf2e0af37c6bb0cd
Antoine <antoine@bannerman.com>
2022-04-04 22:55:15 +0200

    Ss resync (#147)
    * add batch block sync
    
    * update tests

--- functions/lib/tasks.js
+++ functions/lib/tasks.js
@@ -3,8 +3,9 @@ const grpc = require("@grpc/grpc-js");
 const functions = require('firebase-functions');
 
 const TASKS_TO_QUEUE = {
-    blockSyncTask: 'block-sync',
-    transactionSyncTask: 'transaction-sync'
+    batchBlockSyncTask: 'block-sync',
+    transactionSyncTask: 'transaction-sync',
+    blockSyncTask: 'batch-block-sync'
 };
 
 module.exports = {
