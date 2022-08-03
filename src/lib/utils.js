52dba937cbbf27d8c2b694ae3d55d95fefe54d04
Antoine <antoine@bannerman.com>
2022-08-03 11:33:15 +0200

    fix json parameters for methods (#165)
    	modified:   src/lib/utils.js
    	modified:   tests/unit/lib/utils.spec.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,32 +1,5 @@
 const Web3 = require('web3');
 
-export const getPaginatedQuery = function(collection, items, currentOptions, newOptions) {
-    let query;
-    const sortDirection = newOptions.sortDesc[0] === false ? 'asc' : 'desc';
-    const sortByField = newOptions.sortBy[0] || currentOptions.sortBy[0];
-    if (currentOptions.sortDesc[0] != newOptions.sortDesc[0] || !items.length)
-        query = collection
-            .orderBy(sortByField, sortDirection)
-            .limit(newOptions.itemsPerPage);
-    else if (newOptions.page > currentOptions.page)
-        query = collection
-            .orderBy(sortByField, sortDirection)
-            .startAfter(items[items.length - 1][sortByField])
-            .limit(newOptions.itemsPerPage);
-    else if (newOptions.page < currentOptions.page)
-        query = collection
-            .orderBy(sortByField, sortDirection)
-            .endBefore(items[0][sortByField])
-            .limitToLast(newOptions.itemsPerPage);
-    else
-        query = collection
-            .orderBy(sortByField, sortDirection)
-            .startAt(items[0][sortByField])
-            .limit(newOptions.itemsPerPage);
-
-    return query;
-};
-
 export const sanitize = function(obj) {
     return Object.fromEntries(
         Object.entries(obj)
@@ -50,14 +23,8 @@ export const getProvider = function(rpcServer) {
 };
 
 export const processMethodCallParam = function(param, inputType) {
-    if (inputType.endsWith(']') || inputType == 'tuple') {
-        try {
-            return JSON.parse(param);
-        } catch(error) {
-            throw error;
-        }
-    }
- 
+    if (inputType.endsWith(']') || inputType == 'tuple')
+        return JSON.parse(param);
     return param;
 };
 
