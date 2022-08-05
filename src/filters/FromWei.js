0eeb8447363cbffb92cbbdcd47d7725e911899e3
Antoine de Chevigné <antoine@bannerman.com>
2022-08-05 16:59:13 +0200

    remove .0

--- src/filters/FromWei.js
+++ src/filters/FromWei.js
@@ -9,9 +9,11 @@ export default function (amount = 0, to = 'ether', nativeToken = 'ether', decima
 
     const ethAmount = BigNumber.from(String(amount));
     const roundedAmount = formatUnits(ethAmount, decimals)
+    const commified = commify(roundedAmount);
+    const formatted = commified.endsWith('.0') ? commified.split('.')[0] : commified;
 
     if (to == 'ether' && nativeToken != 'ether')
-        return `${commify(roundedAmount)} ${nativeToken}`;
+        return `${formatted} ${nativeToken}`;
     else
         return `${roundedAmount} ${to}`;
 }
