16b2623f6d412a5fcaeae84a7965f62abff0b2c3
Antoine <antoine@bannerman.com>
2022-08-13 10:30:39 +0200

    update teimstamps storing format (#169)

--- tests/unit/components/Blocks.spec.js
+++ tests/unit/components/Blocks.spec.js
@@ -14,18 +14,18 @@ describe('Blocks.vue', () => {
             {
                 number: '2',
                 gasUsed: '200000',
-                timestamp: '1621548470',
+                timestamp: '2022-05-06T17:11:26.000Z',
                 hash: '0x99c6edb3bb1124680a97661c1f5794d60617abb57bd1e611d81fc5b941f36d30'                
             },
             {
                 number: '1',
                 gasUsed: '100000',
-                timestamp: '1621548462',
+                timestamp: '2022-05-06T17:11:26.000Z',
                 hash: '0x98c6edb3bb1124680a97661c1f5794d60617abb57bd1e611d81fc5b941f36d30',
                 transactions: [
                     {
                         hash: '0x060034486a819816df57d01eefccbe161d7019f9f3c235e18af07468fb194ef0',
-                        timestamp: '1621548462',
+                        timestamp: '2022-05-06T17:11:26.000Z',
                         from: '0x0',
                         to: 'Ox1',
                         blockNumber: 1,
