cddd229c0f811226d2441c1e62aa548d99f8e464
Antoine de Chevigné <antoine@bannerman.com>
2022-08-19 22:47:09 +0200

    add ui specs

---
+++ tests/unit/components/ContractVerification.spec.js
@@ -0,0 +1,34 @@
+import MockHelper from '../MockHelper';
+import flushPromises from 'flush-promises'
+
+import ContractVerification from '@/components/ContractVerification.vue';
+
+jest.mock('axios', () => ({
+    get: jest.fn()
+}));
+const axios = require('axios');
+
+const helper = new MockHelper();
+
+describe('ContractVerification.vue', () => {
+    beforeEach(() => jest.clearAllMocks());
+
+    it('Should load the UI & the Solidity releases', async () => {
+        jest.spyOn(axios, 'get')
+            .mockResolvedValue({ data: { builds: [
+                { longVersion: '0.1.2' },
+                { longVersion: '0.1.3', prerelease: 'nightly-0.1.3' },
+            ]}});
+        
+        const wrapper = helper.mountFn(ContractVerification, {
+            getters: {
+                publicExplorer: jest.fn().mockReturnValue({ slug: 'ethernal' })
+            }
+        });
+        await flushPromises();
+        
+        expect(wrapper.vm.allCompilerVersions.length).toEqual(2);
+        expect(wrapper.vm.releasesCompilerVersions.length).toEqual(1);
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+});
