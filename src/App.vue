9fed1d9ff756fcf582bc0b0178b5a193ff615902
Antoine de Chevigné <antoine@bannerman.com>
2022-09-22 17:51:55 +0200

    custom icons for external links

--- src/App.vue
+++ src/App.vue
@@ -107,7 +107,7 @@
                 <v-list dense nav>
                     <v-list-item v-for="(link, idx) in links" target="_blank" :href="link.url" :key="idx">
                         <v-list-item-icon>
-                            <v-icon>mdi-open-in-new</v-icon>
+                            <v-icon>{{ link.icon || 'mdi-open-in-new' }}</v-icon>
                         </v-list-item-icon>
                         <v-list-item-content>
                             <v-list-item-title>{{ link.name }}</v-list-item-title>
