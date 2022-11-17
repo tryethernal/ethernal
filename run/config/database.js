d18177be2b6e869c1631b5f8e96771e4982ecc48
Antoine de Chevigné <antoine@bannerman.com>
2022-11-17 12:06:46 +0100

    move migratiosn

--- run/config/database.js
+++ run/config/database.js
@@ -10,8 +10,8 @@ module.exports = {
     },
     production: {
         "username": process.env.DB_USERNAME,
-        "database": process.env.DB_NAME,
         "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
         "port": process.env.DB_PORT,
         "dialect": "postgres"
