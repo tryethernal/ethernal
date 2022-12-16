aa67430866752363e1f2e01e6806194a018430a9
Antoine de Chevigné <antoine@bannerman.com>
2022-12-16 10:43:33 +0100

    lower logger level

--- run/lib/logger.js
+++ run/lib/logger.js
@@ -1,7 +1,7 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
-    level: 'debug',
+    level: 'error',
     exitOnError: false,
     format: format.json(),
     transports: []
