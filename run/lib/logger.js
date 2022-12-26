fcb2f112c68d030519319e5e796d1b137fa2957c
Antoine de Chevigné <antoine@bannerman.com>
2022-12-26 17:21:29 +0100

    use log shipper

--- run/lib/logger.js
+++ run/lib/logger.js
@@ -7,20 +7,8 @@ const logger = createLogger({
     transports: []
 });
 
-if (process.env.DATADOG_API_KEY) {
-    const httpTransportOptions = {
-        host: 'http-intake.logs.datadoghq.eu',
-        path: `/api/v2/logs?dd-api-key=${process.env.DATADOG_API_KEY}&ddsource=nodejs&service=ethernal`,
-        ssl: true
-    };
-    const transport = new transports.Http(httpTransportOptions);
-    logger.add(transport);
-    logger.exceptions.handle(transport);
-}
-else {
-    const transport = new transports.Console({ format: format.combine(format.colorize(), format.simple()) });
-    logger.add(transport);
-    logger.exceptions.handle(transport);
-}
+const transport = new transports.Console({ format: format.combine(format.colorize(), format.simple()) });
+logger.add(transport);
+logger.exceptions.handle(transport);
 
 module.exports = logger;
