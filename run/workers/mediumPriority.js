3f0189cf1971073e3c9b0018da5f2aa1d9ce32b6
Antoine de Chevigné <antoine@bannerman.com>
2022-12-29 11:40:43 +0100

    more jobs

--- run/workers/mediumPriority.js
+++ run/workers/mediumPriority.js
@@ -8,7 +8,7 @@ priorities['medium'].forEach(jobName => {
     const worker = new Worker(
         jobName,
         async job => await jobs[jobName](job),
-        { concurrency: 500 , connection },
+        { concurrency: 600 , connection },
     );
     worker.on('failed', (job, error) => {
         logger.error(error.message, {
