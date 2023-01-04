7e139b40486f288d72af7b90c33d829ab06800ff
Antoine de Chevigné <antoine@bannerman.com>
2023-01-04 14:55:57 +0100

    add token page

---
+++ src/components/ERC20Contract.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container fluid>
+        <v-tabs v-model="tab">
+            <v-tab id="transactionsTab" href="#transactions">Transactions</v-tab>
+        </v-tabs>
+
+        <v-tabs-items :value="tab">
+            <v-tab-item value="transactions">
+                <Address-Transactions-List :address="address" />
+            </v-tab-item>
+        </v-tabs-items>
+    </v-container>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex';
+
+import AddressTransactionsList from './AddressTransactionsList';
+
+export default {
+    name: 'ERC20Contract',
+    props: ['address'],
+    components: {
+        AddressTransactionsList,
+    },
+    data: () => ({
+    }),
+    methods: {},
+    computed: {
+        ...mapGetters([
+            'currentWorkspace',
+            'isPublicExplorer',
+            'isUserAdmin'
+        ])
+    }
+}
+</script>
+<style scoped>
+</style>
