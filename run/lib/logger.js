62f65527d811dc1fd21f8fb59ce23dfc62d8e54d
Antoine de Chevigné <antoine@bannerman.com>
2023-01-12 12:36:46 +0100

    improve migration

--- run/lib/logger.js
+++ run/lib/logger.js
@@ -1,7 +1,7 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
-    level: 'error',
+    level: 'info',
     exitOnError: false,
     format: format.json(),
     transports: []
