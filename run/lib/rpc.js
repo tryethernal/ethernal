df8b80dd5ce70e0490d0020e18fbcca26fbf09b2
Antoine de Chevigné <antoine@bannerman.com>
2023-01-14 16:23:24 +0100

    fix token balance calculation

--- run/lib/rpc.js
+++ run/lib/rpc.js
@@ -23,7 +23,7 @@ const getBalanceChange = async (address, token, blockNumber, rpcServer) => {
         else
             throw 'Not a big number result'
     } catch(error) {
-        return null;
+        currentBalance = 0;
     }
 
     if (blockNumber > 1) {
@@ -39,7 +39,7 @@ const getBalanceChange = async (address, token, blockNumber, rpcServer) => {
             else
                 throw 'Not a big number result'
         }  catch(error) {
-            return null;
+            previousBalance = 0;
         }
     }
 
