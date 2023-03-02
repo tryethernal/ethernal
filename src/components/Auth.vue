39a73479ef7c17183e1326f7f296572413f8021a
Antoine de Chevigné <antoine@bannerman.com>
2023-03-02 10:38:28 -0300

    remove firebase auth

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -16,9 +16,44 @@
             </v-col>
             <v-col cols="5" class="primary fill-height">
                 <v-row class="fill-height" align-self="center" align="center">
-                    <v-col>
-                        <div id="firebaseui-auth-container"></div>
+                    <v-col cols="2"></v-col>
+                    <v-col cols="8">
+                        <v-card v-if="signInMode" outlined>
+                            <v-card-title>Sign In</v-card-title>
+                            <v-card-text>
+                                <v-form>
+                                    <v-text-field v-model="email" name="email" label="Email" type="text"></v-text-field>
+                                    <v-text-field v-model="password" name="password" label="Password" type="password"></v-text-field>
+                                </v-form>
+                                <v-card-actions class="px-0">
+                                    <div style="float: left;">
+                                        <small><a @click="switchMode()">Sign Up</a></small><br>
+                                        <small><a>Forgot My Password</a></small>
+                                    </div>
+                                    <v-spacer></v-spacer>
+                                    <v-btn :loading="loading" color="primary" @click="signIn()">Sign In</v-btn>
+                                </v-card-actions>
+                            </v-card-text>
+                        </v-card>
+                        <v-card v-else outlined>
+                            <v-card-title>Sign Up</v-card-title>
+                            <v-card-text>
+                                <v-form>
+                                    <v-text-field v-model="email" name="email" label="Email" type="text"></v-text-field>
+                                    <v-text-field v-model="password" name="password" label="Password" type="password"></v-text-field>
+                                </v-form>
+                                <v-card-actions class="px-0">
+                                    <div style="float: left;">
+                                        <small><a @click="switchMode()">Sign In</a></small><br>
+                                        <small><a>Forgot My Password</a></small>
+                                    </div>
+                                    <v-spacer></v-spacer>
+                                    <v-btn :loading="loading" color="primary" @click="signUp()">Sign Up</v-btn>
+                                </v-card-actions>
+                            </v-card-text>
+                        </v-card>
                     </v-col>
+                    <v-col cols="2"></v-col>
                 </v-row>
             </v-col>
         </v-row>
@@ -26,31 +61,29 @@
 </template>
 
 <script>
-import 'firebaseui/dist/firebaseui.css'
-const firebaseui = require('firebaseui');
-
-import { auth } from '../plugins/firebase.js';
-
 export default {
     name: 'Auth',
     data: () => ({
-        carousel: 0,
-        slides: [
-            'Real time transactions',
-            'Contract interactions'
-        ]
+        loading: false,
+        signInMode: true,
+        email: null,
+        password: null
     }),
-    mounted: function() {
-        var ui = new firebaseui.auth.AuthUI(auth());
-        ui.start('#firebaseui-auth-container', {
-            signInSuccessUrl: '/transactions',
-            signInOptions: [
-                {
-                    provider: auth.EmailAuthProvider.PROVIDER_ID,
-                    requireDisplayName: false
-                }
-            ]
-        });
+    methods: {
+        switchMode() {
+            this.email = null;
+            this.password = null;
+            this.signInMode = !this.signInMode;
+        },
+        signIn() {
+            this.server.signIn(this.email, this.password)
+                .then()
+        }
     }
 }
 </script>
+<style scoped>
+a:hover {
+    text-decoration: underline;
+}
+</style>
