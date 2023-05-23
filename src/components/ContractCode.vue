7ad380f161ed089bf16a68f9add5a220f1c4f168
Antoine de Chevigné <antoine@bannerman.com>
2023-05-23 15:25:17 +0200

    reorganize code

--- src/components/ContractCode.vue
+++ src/components/ContractCode.vue
@@ -7,44 +7,8 @@
         </v-card>
         <template v-else>
             <template v-if="isPublicExplorer">
-                <v-card v-if="isVerifiedContract" outlined class="mb-6">
-                    <v-card-text>
-                        <Contract-Verification-Info :contract="contract" />
-                    </v-card-text>
-                </v-card>
-                <v-card v-else outlined class="mb-6">
-                    <v-card-title>Contract Verification</v-card-title>
-                    <v-card-text>
-                        <Contract-Verification :address="contract.address" />
-                    </v-card-text>
-                </v-card>
-
-                <v-card v-if="displayConstructorArguments" outlined class="mb-6">
-                    <v-card-title>
-                        Constructor Arguments
-                        <v-spacer></v-spacer>
-                        <span class="text-caption">
-                            <a :class="{ underlined: formattedConstructorArguments }" @click="formattedConstructorArguments = true">Formatted</a> | <a :class="{ underlined: !formattedConstructorArguments }" @click="formattedConstructorArguments = false">Raw</a>
-                        </span>
-                    </v-card-title>
-                    <v-card-text>
-                        <template v-if="formattedConstructorArguments">
-                            <div v-for="(arg, idx) in decodedConstructorArguments" :key="idx" style="white-space: pre;">
-                                <Formatted-Sol-Var :input="arg" :notInteractive="true" :value="arg.value" />
-                            </div>
-                        </template>
-                        <span v-else>{{ zeroXifiedConstructorArguments }}</span>
-                    </v-card-text>
-                </v-card>
-
-                <v-card v-if="displayLibraries" outlined class="mb-6">
-                    <v-card-title>Libraries</v-card-title>
-                    <v-card-text>
-                        <div v-for="(libraryName, idx) in Object.keys(contract.verification.libraries)" :key="idx">
-                            {{ libraryName }} => {{ contract.verification.libraries[libraryName] }}
-                        </div>
-                    </v-card-text>
-                </v-card>
+                <Contract-Verification-Info v-if="isVerifiedContract" :contract="contract" />
+                <Contract-Verification v-else :address="contract.address" />
             </template>
 
             <v-card outlined class="mb-6">
@@ -77,68 +41,30 @@
 
 <script>
 import 'highlight.js/styles/vs2015.css';
-const ethers = require('ethers');
 const hljs = require('highlight.js');
 import { mapGetters } from 'vuex';
 import ContractVerification from './ContractVerification';
 import ContractVerificationInfo from './ContractVerificationInfo';
-import FormattedSolVar from './FormattedSolVar';
 
 export default {
     name: 'ContractCode',
     props: ['contract'],
     components: {
         ContractVerification,
-        ContractVerificationInfo,
-        FormattedSolVar
+        ContractVerificationInfo
     },
     data: () => ({
-        loading: false,
-        formattedConstructorArguments: true
+        loading: false
     }),
     computed: {
         ...mapGetters([
             'isPublicExplorer'
         ]),
-        zeroXifiedConstructorArguments() {
-            return this.contract.verification.constructorArguments.startsWith('0x') ?
-                this.contract.verification.constructorArguments :
-                `0x${this.contract.verification.constructorArguments}`;
-        },
         isVerifiedContract() {
             return this.contract.verificationStatus == 'success';
         },
         highlightedAsm() {
             return this.contract.asm && hljs.highlight(this.contract.asm, { language: 'x86asm' }).value
-        },
-        displayConstructorArguments() {
-            return this.contract.verification && this.contract.verification.constructorArguments;
-        },
-        displayLibraries() {
-            return this.contract.verification && Object.keys(this.contract.verification.libraries).length > 0;
-        },
-        decodedConstructorArguments() {
-            const iface = new ethers.utils.Interface(this.contract.abi);
-            const constructorInputs = JSON.parse(iface.deploy.format(ethers.utils.FormatTypes.json)).inputs;
-
-            /*
-                This won't handle well tuples in tuples, but hopefully it'll be good enough for now.
-                I'd say that I'll improve later, but we all know it's probably never going to happen
-            */
-            const decodedInputs = ethers.utils.defaultAbiCoder.decode(iface.deploy.inputs.map(i => {
-                if (i.type == 'tuple')
-                    return `tuple(${i.components.map(c => c.type).join(',')})`;
-                else
-                    return i.type;
-            }), this.zeroXifiedConstructorArguments);
-            const decoded = [];
-            for (let i = 0; i < decodedInputs.length; i++) {
-                decoded.push({
-                    ...constructorInputs[i],
-                    value: decodedInputs[i]
-                });
-            }
-            return decoded;
         }
     }
 }
@@ -154,8 +80,5 @@ export default {
     font-weight: 600;
     text-transform: uppercase;
     height: 50vh
-}
-.underlined {
-    text-decoration: underline;
 }
 </style>
--- src/components/ContractVerification.vue
+++ src/components/ContractVerification.vue
@@ -1,172 +1,175 @@
 <template>
-    <div>
-        <v-alert v-if="verificationSuccess" text type="success">Contract has been verified successfully!</v-alert>
-        <v-alert v-if="verificationErrorMessage" text type="error">{{ verificationErrorMessage }}</v-alert>
-        <v-form ref="form" v-model="canSubmit">
-            <h4>Compiler Settings</h4>
-            <v-card outlined class="mb-2">
-                <v-card-text>
-                    <v-row>
-                        <v-col cols="4">
-                            <v-select
-                                outlined
-                                dense
-                                label="Compiler Version *"
-                                v-model="parameters.compiler"
-                                :items="includeNightlyBuilds ? allCompilerVersions : releasesCompilerVersions"
-                                hide-details
-                                return-object>
-                            </v-select>
-                        </v-col>
+    <v-card outlined class="mb-6">
+        <v-card-title>Contract Verification</v-card-title>
+        <v-card-text>
+            <v-alert v-if="verificationSuccess" text type="success">Contract has been verified successfully!</v-alert>
+            <v-alert v-if="verificationErrorMessage" text type="error">{{ verificationErrorMessage }}</v-alert>
+            <v-form ref="form" v-model="canSubmit">
+                <h4>Compiler Settings</h4>
+                <v-card outlined class="mb-2">
+                    <v-card-text>
+                        <v-row>
+                            <v-col cols="4">
+                                <v-select
+                                    outlined
+                                    dense
+                                    label="Compiler Version *"
+                                    v-model="parameters.compiler"
+                                    :items="includeNightlyBuilds ? allCompilerVersions : releasesCompilerVersions"
+                                    hide-details
+                                    return-object>
+                                </v-select>
+                            </v-col>
 
-                        <v-col cols="3">
-                            <v-checkbox
-                                dense
-                                label="Show nightly builds"
-                                v-model="includeNightlyBuilds"
-                                hide-details>
-                            </v-checkbox>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="4">
-                            <v-select
-                                outlined
-                                dense
-                                label="EVM Version *"
-                                v-model="parameters.evmVersion"
-                                :items="evmVersions"
-                                item-text="text"
-                                item-value="value"
-                                hide-details>
-                            </v-select>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="2">
-                            <v-select
-                                outlined
-                                dense
-                                label="Optimization *"
-                                v-model="parameters.optimizer"
-                                :items="[{ text: 'Yes', value: true }, { text: 'No', value: false }]"
-                                item-text="text"
-                                item-value="value"
-                                hide-details>
-                            </v-select>
-                        </v-col>
-                        <v-col cols="2">
-                            <v-text-field
-                                v-show="parameters.optimizer"
-                                v-model.number="parameters.runs"
-                                small
-                                outlined
-                                dense
-                                hide-details
-                                label="Runs *"
-                                type="number">
-                            </v-text-field>
-                        </v-col>
-                    </v-row>
-                </v-card-text>
-            </v-card>
+                            <v-col cols="3">
+                                <v-checkbox
+                                    dense
+                                    label="Show nightly builds"
+                                    v-model="includeNightlyBuilds"
+                                    hide-details>
+                                </v-checkbox>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="4">
+                                <v-select
+                                    outlined
+                                    dense
+                                    label="EVM Version *"
+                                    v-model="parameters.evmVersion"
+                                    :items="evmVersions"
+                                    item-text="text"
+                                    item-value="value"
+                                    hide-details>
+                                </v-select>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="2">
+                                <v-select
+                                    outlined
+                                    dense
+                                    label="Optimization *"
+                                    v-model="parameters.optimizer"
+                                    :items="[{ text: 'Yes', value: true }, { text: 'No', value: false }]"
+                                    item-text="text"
+                                    item-value="value"
+                                    hide-details>
+                                </v-select>
+                            </v-col>
+                            <v-col cols="2">
+                                <v-text-field
+                                    v-show="parameters.optimizer"
+                                    v-model.number="parameters.runs"
+                                    small
+                                    outlined
+                                    dense
+                                    hide-details
+                                    label="Runs *"
+                                    type="number">
+                                </v-text-field>
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+                </v-card>
 
-            <h4>Contract</h4>
-            <v-card outlined class="mb-2">
-                <v-card-text>
-                    <v-row>
-                        <v-col cols="4" class="pb-0">
-                            <v-text-field
-                                :rules="[v => !!v || 'Contract name is required']"
-                                v-model="parameters.name"
-                                small
-                                outlined
-                                dense
-                                label="Contract Name *">
-                            </v-text-field>
-                        </v-col>
-                    </v-row>
+                <h4>Contract</h4>
+                <v-card outlined class="mb-2">
+                    <v-card-text>
+                        <v-row>
+                            <v-col cols="4" class="pb-0">
+                                <v-text-field
+                                    :rules="[v => !!v || 'Contract name is required']"
+                                    v-model="parameters.name"
+                                    small
+                                    outlined
+                                    dense
+                                    label="Contract Name *">
+                                </v-text-field>
+                            </v-col>
+                        </v-row>
 
-                    <v-row>
-                        <v-col cols="6" class="pt-0">
-                            <v-file-input
-                                v-model="files"
-                                :rules="[v => !!v || 'At least one file is required.', v => v && v.length > 0 || 'At least one file is required.']"
-                                :hint="'To add multiple files, select them all at once.'"
-                                persistent-hint
-                                accept=".sol"
-                                small-chips
-                                multiple
-                                outlined
-                                show-size
-                                counter
-                                label="Contract Files *"
-                                prepend-inner-icon="mdi-paperclip"
-                                prepend-icon=""
-                                @change="onFilesUploaded">
-                            </v-file-input>
-                        </v-col>
-                    </v-row>
-                </v-card-text>
-            </v-card>
+                        <v-row>
+                            <v-col cols="6" class="pt-0">
+                                <v-file-input
+                                    v-model="files"
+                                    :rules="[v => !!v || 'At least one file is required.', v => v && v.length > 0 || 'At least one file is required.']"
+                                    :hint="'To add multiple files, select them all at once.'"
+                                    persistent-hint
+                                    accept=".sol"
+                                    small-chips
+                                    multiple
+                                    outlined
+                                    show-size
+                                    counter
+                                    label="Contract Files *"
+                                    prepend-inner-icon="mdi-paperclip"
+                                    prepend-icon=""
+                                    @change="onFilesUploaded">
+                                </v-file-input>
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+                </v-card>
 
-            <h4>Libraries Linking</h4>
-            <v-card outlined class="mb-2">
-                <v-card-text>
-                    <v-row v-for="(library, index) in rawLibraries" :key="index" align="center">
-                        <v-col cols="3" :class="{ 'pb-1': index == 0, 'py-0': index > 0 }">
-                            <v-text-field
-                                v-model="rawLibraries[index].name"
-                                :rules="[v => !!v] || 'Library name is required'"
-                                small
-                                outlined
-                                dense
-                                persistent-hint
-                                hint="Format: MyFile.sol:LibraryName"
-                                label="Library Name *">
-                            </v-text-field>
-                        </v-col>
-                        <v-col cols="4" :class="{ 'pb-1': index == 0, 'py-0': index > 0 }">
-                            <v-text-field
-                                v-model="rawLibraries[index].address"
-                                :rules="[v => !!v && v.length == 42 || 'Invalid address (must be 42 characters long)']"
-                                small
-                                outlined
-                                dense
-                                label="Library Address *">
-                                    <template v-slot:append-outer>
-                                        <v-btn icon small  @click="removeLibrary(index)">
-                                            <v-icon color="error">mdi-delete</v-icon>
-                                        </v-btn>
-                                    </template>
-                            </v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="2">
-                            <v-btn class="primary" @click="addLibrary()" depressed>Add Library</v-btn>
-                        </v-col>
-                    </v-row>
-                </v-card-text>
-            </v-card>
+                <h4>Libraries Linking</h4>
+                <v-card outlined class="mb-2">
+                    <v-card-text>
+                        <v-row v-for="(library, index) in rawLibraries" :key="index" align="center">
+                            <v-col cols="3" :class="{ 'pb-1': index == 0, 'py-0': index > 0 }">
+                                <v-text-field
+                                    v-model="rawLibraries[index].name"
+                                    :rules="[v => !!v] || 'Library name is required'"
+                                    small
+                                    outlined
+                                    dense
+                                    persistent-hint
+                                    hint="Format: MyFile.sol:LibraryName"
+                                    label="Library Name *">
+                                </v-text-field>
+                            </v-col>
+                            <v-col cols="4" :class="{ 'pb-1': index == 0, 'py-0': index > 0 }">
+                                <v-text-field
+                                    v-model="rawLibraries[index].address"
+                                    :rules="[v => !!v && v.length == 42 || 'Invalid address (must be 42 characters long)']"
+                                    small
+                                    outlined
+                                    dense
+                                    label="Library Address *">
+                                        <template v-slot:append-outer>
+                                            <v-btn icon small  @click="removeLibrary(index)">
+                                                <v-icon color="error">mdi-delete</v-icon>
+                                            </v-btn>
+                                        </template>
+                                </v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="2">
+                                <v-btn class="primary" @click="addLibrary()" depressed>Add Library</v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+                </v-card>
 
-            <h4>Constructor Arguments</h4>
-            <v-card outlined class="mb-2">
-                <v-card-text>
-                    <v-textarea
-                        rows="3"
-                        primary
-                        hide-details
-                        outlined
-                        dense
-                        v-model="parameters.constructorArguments"
-                        label="Constructor Arguments (ABI Encoded)">
-                    </v-textarea>
-                </v-card-text>
-            </v-card>
-        </v-form>
-        <v-btn class="primary" :disabled="!canSubmit" :loading="loading" depressed @click="submit()">Verify</v-btn>
-    </div>
+                <h4>Constructor Arguments</h4>
+                <v-card outlined class="mb-2">
+                    <v-card-text>
+                        <v-textarea
+                            rows="3"
+                            primary
+                            hide-details
+                            outlined
+                            dense
+                            v-model="parameters.constructorArguments"
+                            label="Constructor Arguments (ABI Encoded)">
+                        </v-textarea>
+                    </v-card-text>
+                </v-card>
+            </v-form>
+            <v-btn class="primary" :disabled="!canSubmit" :loading="loading" depressed @click="submit()">Verify</v-btn>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
