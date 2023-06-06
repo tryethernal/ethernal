a41803ee7b7e3c25f1278ec30332157cec9f3d9b
Antoine de Chevigné <antoine@bannerman.com>
2023-06-06 17:14:59 +0200

    improve queuing

--- run/lib/queue.js
+++ run/lib/queue.js
@@ -4,8 +4,8 @@ const { sanitize } = require("./utils");
 const uniqueQueues = ["blockSync", "transactionSync", "integrityCheck", "integrityCheckStarter", "rpcHealthCheck", "rpcHealthCheckStarter", "batchBlockSync"];
 
 const enqueue = (queueName, jobName, data, priority = 10, repeat) => {
-    const timedJobName = uniqueQueues.indexOf(queueName) < 0 ? `${jobName}-${Date.now()}` : null;
-    return queues[queueName].add(timedJobName || jobName, data, sanitize({ priority, repeat, jobId: timedJobName }));
+    const jobId = uniqueQueues.indexOf(queueName) > -1 ? jobName : null;
+    return queues[queueName].add(jobName || jobName, data, sanitize({ priority, repeat, jobId: jobId }));
 };
 
 const bulkEnqueue = (queueName, jobData) => {
