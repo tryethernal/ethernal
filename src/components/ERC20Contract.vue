19ecdcce17562679b2a3a0adcfec14fc6f3f73d4
Antoine <adechevigne@gmail.com>
2023-07-05 09:14:17 -0600

    fix tests (#267)

--- src/components/ERC20Contract.vue
+++ src/components/ERC20Contract.vue
@@ -59,7 +59,7 @@
                     </v-col>
 
                     <v-col cols="12" sm="6" lg="6">
-                        <Stat-Number :loading="loadingStats" :title="'Circulating Supply'" :value="contractStats.tokenCirculatingSupply" :decimals="contract.tokenDecimals" :infoTooltip="'Number of minted tokens'" />
+                        <Stat-Number :loading="loadingStats" :title="'Circulating Supply'" :value="contractStats.tokenCirculatingSupply" :decimals="contract.tokenDecimals" :infoTooltip="'Number of tokens currently in circulation'" />
                     </v-col>
 
                     <v-col cols="12" sm="6" lg="6">
