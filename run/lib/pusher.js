4d62d857f93c2601b8c118f5727adbff017fb729
Antoine de Chevigné <antoine@bannerman.com>
2023-08-21 00:08:10 +0200

    use falgs

--- run/lib/pusher.js
+++ run/lib/pusher.js
@@ -1,9 +1,9 @@
 const Pusher = require('pusher');
 const logger = require('./logger');
+const { isPusherEnabled } = require('./flags');
 
-const isPusherPresent = false;
 
-const pusher = isPusherPresent ?
+const pusher = isPusherEnabled() ?
     new Pusher({
         appId: process.env.PUSHER_APP_ID,
         key: process.env.PUSHER_KEY,
@@ -15,7 +15,7 @@ const pusher = isPusherPresent ?
 
 module.exports = {
     trigger: (channel, event, data) => {
-        if (isPusherPresent) {
+        if (isPusherEnabled()) {
             pusher.trigger(channel, event, data)
                 .catch(error => logger.error(error.message, { location: 'lib.pusher', error, channel, event, data }))
         }
