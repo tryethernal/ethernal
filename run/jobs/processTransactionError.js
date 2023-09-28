153df8823afe4c77cf9d8c419be52b638422642e
Antoine de Chevigné <antoine@bannerman.com>
2023-09-28 16:32:55 +0200

    more tests

--- run/jobs/processTransactionError.js
+++ run/jobs/processTransactionError.js
@@ -23,7 +23,7 @@ module.exports = async job => {
         return 'Sync is disabled';
 
     if (transaction.workspace.rpcHealthCheck && !transaction.workspace.rpcHealthCheck.isReachable)
-        return 'Too many failed RPC requests';
+        return 'RPC is not reachable';
 
     if (!transaction.workspace.explorer.stripeSubscription)
         return 'No active subscription';
