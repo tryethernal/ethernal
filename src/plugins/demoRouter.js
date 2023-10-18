fe4160306a939c183b8176bf09b9e09c6f339e35
Antoine de Chevigné <antoine@bannerman.com>
2023-10-18 16:54:54 +0200

    frontend tests

--- src/plugins/demoRouter.js
+++ src/plugins/demoRouter.js
@@ -1,10 +1,8 @@
 import VueRouter from 'vue-router';
 import DemoExplorerSetup from '../components/DemoExplorerSetup.vue';
-import DemoExplorerUpgrade from '../components/DemoExplorerUpgrade.vue';
 
 const routes = [
     { path: '/demo', component: DemoExplorerSetup },
-    { path: '/demo/upgradeExplorer', component: DemoExplorerUpgrade },
     { path: '*', redirect: '/demo' }
 ];
 
