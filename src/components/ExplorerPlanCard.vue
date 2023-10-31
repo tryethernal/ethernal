e5653163601957b8d4be43c0192d680384189027
Antoine de Chevigné <antoine@bannerman.com>
2023-10-31 14:16:31 +0100

    fix tests + better demo setup page

--- src/components/ExplorerPlanCard.vue
+++ src/components/ExplorerPlanCard.vue
@@ -8,8 +8,11 @@
         </v-card-title>
         <v-card-subtitle class="pb-0">${{ plan.price.toLocaleString() }} / month</v-card-subtitle>
         <v-card-text>
-            <v-list dense disabled>
+            <v-list dense>
                 <v-list-item>
+                    <v-list-item-icon class="mr-2">
+                        <v-icon color="success">mdi-check</v-icon>
+                    </v-list-item-icon>
                     <v-list-item-content class="py-0">
                         <v-list-item-title style="font-weight: normal;">
                             {{ plan.capabilities.txLimit.toLocaleString() }} txs / month
@@ -17,57 +20,103 @@
                     </v-list-item-content>
                 </v-list-item>
                 <v-list-item>
+                    <v-list-item-icon class="mr-2">
+                        <v-icon color="success">mdi-check</v-icon>
+                    </v-list-item-icon>
                     <v-list-item-content>
                         <v-list-item-title style="font-weight: normal;">
-                            Data Retention: {{ plan.capabilities.dataRetention > 0 ? `${plan.capabilities.dataRetention} days` : 'Unlimited' }}
+                            {{ plan.capabilities.dataRetention > 0 ? `${plan.capabilities.dataRetention} days` : 'Unlimited' }} Data Retention
+                            <v-tooltip left color="black">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-icon v-bind="attrs" v-on="on" class="ml-1" small>mdi-help-circle</v-icon>
+                                </template>
+                                <template v-if="plan.capabilities.dataRetention > 0">
+                                    Your data will automatically be deleted after {{ plan.capabilities.dataRetention }} days.
+                                </template>
+                                <template v-else>
+                                    Your data will never be deleted automatically.
+                                </template>
+                            </v-tooltip>
                         </v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
                 <v-list-item>
-                    <v-list-item-icon>
+                    <v-list-item-icon class="mr-2">
                         <v-icon :color="pickIconColor(plan.capabilities.customDomain)">{{ pickIcon(plan.capabilities.customDomain) }}</v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
-                        <v-list-item-title style="font-weight: normal;">Custom Domain</v-list-item-title>
+                        <v-list-item-title style="font-weight: normal;">
+                            Custom Domains
+                            <v-tooltip left color="black">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-icon v-bind="attrs" v-on="on" class="ml-1" small>mdi-help-circle</v-icon>
+                                </template>
+                                Add unlimited custom domain names to access your explorer.
+                            </v-tooltip>
+                        </v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
                 <v-list-item>
-                    <v-list-item-icon>
+                    <v-list-item-icon class="mr-2">
                         <v-icon :color="pickIconColor(plan.capabilities.nativeToken)">{{ pickIcon(plan.capabilities.nativeToken) }}</v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
                         <v-list-item-title style="font-weight: normal;">
-                            Native Token
+                            Native Token Symbol
+                            <v-tooltip left color="black">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-icon v-bind="attrs" v-on="on" class="ml-1" small>mdi-help-circle</v-icon>
+                                </template>
+                                Display your native token symbol instead of "ether".
+                            </v-tooltip>
                         </v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
                 <v-list-item>
-                    <v-list-item-icon>
+                    <v-list-item-icon class="mr-2">
                         <v-icon :color="pickIconColor(plan.capabilities.totalSupply)">{{ pickIcon(plan.capabilities.totalSupply) }}</v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
                         <v-list-item-title style="font-weight: normal;">
                             Total Supply
+                            <v-tooltip left color="black">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-icon v-bind="attrs" v-on="on" class="ml-1" small>mdi-help-circle</v-icon>
+                                </template>
+                                Display your native token total supply.
+                            </v-tooltip>
                         </v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
                 <v-list-item>
-                    <v-list-item-icon>
+                    <v-list-item-icon class="mr-2">
                         <v-icon :color="pickIconColor(plan.capabilities.statusPage)">{{ pickIcon(plan.capabilities.statusPage) }}</v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
                         <v-list-item-title style="font-weight: normal;">
                             Status Page
+                            <v-tooltip left color="black">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-icon v-bind="attrs" v-on="on" class="ml-1" small>mdi-help-circle</v-icon>
+                                </template>
+                                Add a status page that displays the status (up/down) of your RPC.
+                            </v-tooltip>
                         </v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
                 <v-list-item>
-                    <v-list-item-icon>
+                    <v-list-item-icon class="mr-2">
                         <v-icon :color="pickIconColor(plan.capabilities.branding)">{{ pickIcon(plan.capabilities.branding) }}</v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
                         <v-list-item-title style="font-weight: normal;">
                             Whitelabeling
+                            <v-tooltip left color="black">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-icon v-bind="attrs" v-on="on" class="ml-1" small>mdi-help-circle</v-icon>
+                                </template>
+                                Customize colors, font, banner, logo, favicons, add external links.
+                            </v-tooltip>
                         </v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
