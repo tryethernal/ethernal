2aaddae6327dcca8ed2eb814d71a9b5f7fae115d
Antoine de Chevigné <antoine@bannerman.com>
2023-11-30 16:21:09 +0100

    better quota check

--- run/jobs/blockSync.js
+++ run/jobs/blockSync.js
@@ -49,9 +49,8 @@ module.exports = async job => {
         if (!block)
             throw new Error("Couldn't fetch block from provider");
 
-        const quota = workspace.explorer.stripeSubscription.stripePlan.capabilities.txLimit;
-        const currentTransactionCount = workspace.explorer.stripeSubscription.transactionQuota;
-        if (currentTransactionCount >= quota)
+        const explorer = await workspace.getExplorer();
+        if (await explorer.hasReachedTransactionQuota())
             return 'Transaction quota reached.';
 
         const syncedBlock = await db.syncPartialBlock(workspace.id, block);
