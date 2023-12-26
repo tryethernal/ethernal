6a3936c104e44f1a5b70581b5a62f15ec40674fa
Antoine de Chevigné <antoine@bannerman.com>
2023-12-26 22:05:51 +0100

    update rpc check starter

--- run/tests/jobs/rpcHealthCheckStarter.test.js
+++ run/tests/jobs/rpcHealthCheckStarter.test.js
@@ -1,4 +1,4 @@
-const { Workspace } = require('../mocks/models');
+const { Workspace } = require('../mocks/models');
 require('../mocks/lib/queue');
 
 const { enqueue } = require('../../lib/queue');
@@ -7,15 +7,15 @@ const rpcHealthCheckStarter = require('../../jobs/rpcHealthCheckStarter');
 beforeEach(() => jest.clearAllMocks());
 
 describe('rpcHealthCheckStarter', () => {
-    it('Should enqueue integrity check processes if integrityCheck start number is set', async () => {
-        jest.spyOn(Workspace, 'findAll').mockResolvedValueOnce([
-            { id: 2, rpcHealthCheckEnabled: true }
-        ]);
+    it('Should enqueue integrity check processes if integrityCheck start number is set', (done) => {
+        jest.spyOn(Workspace, 'findAll').mockResolvedValueOnce([{ id: 2 }]);
 
-        await rpcHealthCheckStarter({});
-
-        expect(enqueue).toHaveBeenNthCalledWith(1, 'rpcHealthCheck', 'rpcHealthCheck-2', {
-            workspaceId: 2,
-        });
+        rpcHealthCheckStarter()
+            .then(() => {
+                expect(enqueue).toHaveBeenNthCalledWith(1, 'rpcHealthCheck', 'rpcHealthCheck-2', {
+                    workspaceId: 2,
+                });
+                done();
+            });
     });
 });
