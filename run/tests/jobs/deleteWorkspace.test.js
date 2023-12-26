8e7f5a601019693179467d3d1fc9b0961436db17
Antoine de Chevigné <antoine@bannerman.com>
2023-12-26 19:56:49 +0100

    more tests

---
+++ run/tests/jobs/deleteWorkspace.test.js
@@ -0,0 +1,57 @@
+require('../mocks/lib/queue');
+require('../mocks/lib/env');
+const { Workspace } = require('../mocks/models');
+const { enqueue } = require('../../lib/queue');
+const env = require('../../lib/env');
+
+const deleteWorkspace = require('../../jobs/deleteWorkspace');
+
+beforeEach(() => jest.clearAllMocks());
+
+jest.spyOn(env, 'getMaxBlockForSyncReset').mockReturnValue(2);
+
+describe('cancelDemoExplorers', () => {
+    it('Should return if cannot find workspace', (done) => {
+        jest.spyOn(Workspace, 'findByPk').mockResolvedValueOnce(null);
+
+        deleteWorkspace({ data: { workspaceId: 1 }})
+            .then(res => {
+                expect(res).toEqual('Cannot find workspace');
+                done();
+            });
+    });
+
+    it('Should return if workspace is not waiting to be deleted', (done) => {
+        jest.spyOn(Workspace, 'findByPk').mockResolvedValueOnce({ pendingDeletion: false });
+
+        deleteWorkspace({ data: { workspaceId: 1 }})
+            .then(res => {
+                expect(res).toEqual('This workspace has not been marked for deletion');
+                done();
+            });
+    });
+
+    it('Should reenqueue if too many blocks to delete now', (done) => {
+        const getBlocks = jest.fn().mockResolvedValueOnce([{}, {}]);
+        jest.spyOn(Workspace, 'findByPk').mockResolvedValueOnce({ id: 1, getBlocks, pendingDeletion: true });
+
+        deleteWorkspace({ data: { workspaceId: 1 }})
+            .then(res => {
+                expect(enqueue).toHaveBeenCalledWith('deleteWorkspace', 'deleteWorkspace-1', { workspaceId: 1 }, 1, null, 3600000);
+                expect(res).toEqual('Too many blocks for deletion');
+                done();
+            });
+    });
+
+    it('Should delete workspace now', (done) => {
+        const getBlocks = jest.fn().mockResolvedValueOnce([{}]);
+        const safeDelete = jest.fn();
+        jest.spyOn(Workspace, 'findByPk').mockResolvedValueOnce({ id: 1, safeDelete, getBlocks, pendingDeletion: true });
+
+        deleteWorkspace({ data: { workspaceId: 1 }})
+            .then(() => {
+                expect(safeDelete).toHaveBeenCalled();
+                done();
+            });
+    });
+});
