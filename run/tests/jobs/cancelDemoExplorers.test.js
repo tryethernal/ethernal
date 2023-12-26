c9b2579a40e3ede4d2dae23c354bb40abea12954
Antoine de Chevigné <antoine@bannerman.com>
2023-12-26 18:56:27 +0100

    backedn test

--- run/tests/jobs/cancelDemoExplorers.test.js
+++ run/tests/jobs/cancelDemoExplorers.test.js
@@ -7,23 +7,36 @@ const cancelDemoExplorers = require('../../jobs/cancelDemoExplorers');
 beforeEach(() => jest.clearAllMocks());
 
 const safeDeleteSubscription = jest.fn();
+const safeDelete = jest.fn();
+const update = jest.fn();
 
 describe('cancelDemoExplorers', () => {
-    it('Should delete subscription & enqueue syncing process update', (done) => {
+    it('Should delete subscription, explorer, update workspace & enqueue ws reset & deletion', (done) => {
+        jest.useFakeTimers()
+            .setSystemTime(new Date('2023-12-26'));
+
         jest.spyOn(Explorer, 'findAll').mockResolvedValue([
             {
+                workspaceId: 1,
                 slug: 'slug',
                 stripeSubscription: { stripeId: '123' },
-                safeDeleteSubscription
+                safeDeleteSubscription,
+                safeDelete,
+                workspace: { update }
             }
         ]);
 
         cancelDemoExplorers()
             .then(res => {
                 expect(safeDeleteSubscription).toHaveBeenCalledWith('123');
-                expect(enqueue).toBeCalledWith('updateExplorerSyncingProcess', 'updateExplorerSyncingProcess-slug', {
-                    explorerSlug: 'slug'
+                expect(safeDelete).toHaveBeenCalled();
+                expect(update).toHaveBeenCalledWith({ pendingDeletion: true, public: false });
+                expect(enqueue).toHaveBeenCalledWith('workspaceReset', 'workspaceReset-1', {
+                    workspaceId: 1,
+                    from: new Date(0),
+                    to: new Date()
                 });
+                expect(enqueue).toHaveBeenCalledWith('deleteWorkspace', 'deleteWorkspace-1', { workspaceId: 1 });
                 expect(res).toEqual(['slug']);
                 done();
             });
