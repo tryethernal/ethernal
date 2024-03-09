86a90a14f725140e5f843f880e523b50cd98f53a
Antoine de Chevigné <antoine@bannerman.com>
2024-03-09 19:45:44 +0100

    use soketi instead of pusher

--- run/lib/pusher.js
+++ run/lib/pusher.js
@@ -1,19 +1,22 @@
 const Pusher = require('pusher');
 const logger = require('./logger');
 const { isPusherEnabled, isProductionEnvironment } = require('./flags');
-
+const { getSoketiDefaultAppId, getSoketiDefaultAppKey, getSoketiDefaultAppSecret, getSoketiHost, getSoketiPort, getSoketiUseTLS } = require('./env');
 
 const pusher = isPusherEnabled() ?
     new Pusher({
-        appId: process.env.PUSHER_APP_ID,
-        key: process.env.PUSHER_KEY,
-        secret: process.env.PUSHER_SECRET,
-        cluster: 'eu',
-        useTLS: true
+        appId: getSoketiDefaultAppId(),
+        key: getSoketiDefaultAppKey(),
+        secret: getSoketiDefaultAppSecret(),
+        host: getSoketiHost(),
+        port: getSoketiPort(),
+        scheme: 'http',
+        useTLS: getSoketiUseTLS()
     }) :
     { trigger: () => new Promise(resolve => resolve()) };
 
 module.exports = {
+    pusher,
     trigger: (channel, event, data) => {
         if (isPusherEnabled()) {
             pusher.trigger(channel, event, data)
