72ef42d62fc6a180df54b220df5150d1b144b389
Antoine de Chevigné <antoine@bannerman.com>
2024-04-12 11:46:55 +0200

    backend tests

--- run/tests/api/erc721Collections.test.js
+++ run/tests/api/erc721Collections.test.js
@@ -1,13 +1,33 @@
 require('../mocks/lib/queue');
 require('../mocks/lib/firebase');
+require('../mocks/lib/rpc');
 require('../mocks/middlewares/workspaceAuth');
 const db = require('../../lib/firebase');
+
+const { ERC721Connector } = require('../../lib/rpc');
 
 const supertest = require('supertest');
 const app = require('../../app');
 const request = supertest(app);
 
 const BASE_URL = '/api/erc721Collections';
+
+describe(`GET ${BASE_URL}/:address/totalSupply`, () => {
+    beforeEach(() => jest.clearAllMocks());
+
+    it('Should return collection total supply', (done) => {
+        ERC721Connector.mockImplementation(() => ({
+            totalSupply: jest.fn().mockResolvedValueOnce('10000'),
+        }));
+        request.get(`${BASE_URL}/0x123/totalSupply`)
+            .expect(200)
+            .then(({ body }) => {
+                expect(body).toEqual({ totalSupply: '10000' });
+                done();
+            });
+    });
+});
+
 
 describe(`GET ${BASE_URL}/:address/tokens`, () => {
     beforeEach(() => jest.clearAllMocks());
