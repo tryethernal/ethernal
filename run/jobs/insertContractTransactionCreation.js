5121dc13a9df50a8c799220a5ee10097d362e00b
Antoine de Chevigné <antoine@tryethernal.com>
2024-08-15 00:13:18 +0200

    better job insertion

--- run/jobs/insertContractTransactionCreation.js
+++ run/jobs/insertContractTransactionCreation.js
@@ -3,10 +3,13 @@ const { Contract } = require('../models');
 module.exports = async job => {
     const data = job.data;
 
-    if (!data.contractId || !data.transactionId)
+    if (!data.workspaceId || !data.contractAddress || !data.transactionId)
         return 'Missing parameter';
 
-    return Contract.update({ transactionId: data.transactionId}, {
-        where: { id: data.contractId }
+    return Contract.update({ transactionId: data.transactionId }, {
+        where: {
+            workspaceId: data.workspaceId,
+            address: data.contractAddress
+        }
     });
 };
