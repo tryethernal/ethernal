7dd68a172408a0847a687dc76aadc3b887bf0478
Antoine de Chevigné <antoine@tryethernal.com>
2024-08-19 16:20:54 +0200

    remove

--- run/lib/logger.js
+++ run/lib/logger.js
@@ -1,7 +1,7 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
-    level: 'debug',
+    level: process.env.LOG_LEVEL || 'info',
     exitOnError: false,
     format: format.json(),
     transports: []
