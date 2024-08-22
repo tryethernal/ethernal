a0abef2d0e7d07be82d34418c812c25f6fe8672b
Antoine de Chevigné <antoine@tryethernal.com>
2024-08-22 16:27:47 +0200

    try fixing sentry upload

--- vue.config.js
+++ vue.config.js
@@ -42,7 +42,7 @@ module.exports = {
             new WorkerPlugin(),
             sentryWebpackPlugin({
                 debug: true,
-                url: process.env.SENTRY_URL,
+                sentryUrl: process.env.SENTRY_URL,
                 authToken: process.env.SENTRY_AUTH_TOKEN,
                 org: process.env.SENTRY_ORG,
                 project: process.env.SENTRY_PROJECT,
