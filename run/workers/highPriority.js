e17bc1c83aa2945ad58a1f15a9b025d25aa2df33
Antoine <adechevigne@gmail.com>
2024-08-26 11:49:07 +0200

    integrate sentry in workers (#370)
    Co-authored-by: Antoine de Chevigné <antoine@tryethernal.com>

--- run/workers/highPriority.js
+++ run/workers/highPriority.js
@@ -1,29 +1,13 @@
+require('../lib/instrument');
 const { initializeApp } = require('firebase-admin/app');
-const { getNodeEnv, getSentryDsn, getVersion } = require('../lib/env');
-const Sentry = require('@sentry/node');
-
-if (getSentryDsn()) {
-    const { nodeProfilingIntegration } = require('@sentry/profiling-node');
-    Sentry.init({
-        dsn: getSentryDsn(),
-        environment: getNodeEnv() || 'development',
-        release: `ethernal@${getVersion()}`,
-        skipOpenTelemetrySetup: true,
-        integrations: [
-            nodeProfilingIntegration(),
-            Sentry.postgresIntegration
-        ],
-        tracesSampleRate: 1.0,
-        profilesSampleRate: 1.0
-    });
-}
+const { getNodeEnv } = require('../lib/env');
 initializeApp();
-
 const { Worker } = require('bullmq');
-const connection = require('../config/redis')[process.env.NODE_ENV || 'production'];
+const connection = require('../config/redis')[getNodeEnv()];
 const jobs = require('../jobs');
 const logger = require('../lib/logger');
 const priorities = require('./priorities.json');
+const { managedWorkerError } = require('../lib/errors');
 
 priorities['high'].forEach(jobName => {
     const worker = new Worker(
@@ -31,18 +15,9 @@ priorities['high'].forEach(jobName => {
         async job => await jobs[jobName](job),
         { concurrency: 200, maxStalledCount: 5, connection },
     );
-    worker.on('failed', (job, error) => {
-        Sentry.setTag('worker', 'highPriority');
-        Sentry.setTag('job', jobName);
-        Sentry.setContext('Job Data', job.data);
-        Sentry.captureException(error);
-        return logger.error(error.message, {
-            location: `workers.highPriority.${jobName}`,
-            error: error,
-            data: job.data 
-        });
-    });
-    if (process.env.NODE_ENV == 'production')
+    worker.on('failed', (job, error) => managedWorkerError(error, jobName, job.data, 'highPriority'));
+
+    if (getNodeEnv() == 'production')
         logger.info(`Started worker "${jobName}" - Priority: high`);
     else
         console.log(`Started worker "${jobName}" - Priority: high`);
