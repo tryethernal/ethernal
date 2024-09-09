a51b6ff2ffe6f7a430be4e31066855d64dc39d8e
Antoine de Chevigné <antoine@tryethernal.com>
2024-09-09 20:15:13 +0200

    fix scietnific notation conversion

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3');
 const ethers = require('ethers');
-const BigNumber = ethers.BigNumber;
+// const BigNumber = ethers.BigNumber;
 
 // https://stackoverflow.com/a/22885197/1373409
 export const getSignificantDigitCount = (n) => {
@@ -30,35 +30,35 @@ export const BNtoSignificantDigits = (num, digits = 4) => {
     return parseFloat((+ethers.utils.formatEther(num)).toPrecision(digits));
 };
 
-/******************************************************************
- * Converts e-Notation Numbers to Plain Numbers
- ******************************************************************
- * From: https://stackoverflow.com/a/66072001/1373409
- * @function eToNumber(number)
- * @version  1.00
- * @param   {e nottation Number} valid Number in exponent format.
- *          pass number as a string for very large 'e' numbers or with large fractions
- *          (none 'e' number returned as is).
- * @return  {string}  a decimal number string.
- * @author  Mohsen Alyafei
- * @date    17 Jan 2020
- * Note: No check is made for NaN or undefined input numbers.
- *
- *****************************************************************/
- export const eToNumber = (num) => {
+export const eToNumber = (num) => {
     let sign = "";
-    (num += "").charAt(0) == "-" && (num = num.substring(1), sign = "-");
-    let arr = num.split(/[e]/ig);
+    (num += "").charAt(0) === "-" && (num = num.substring(1), sign = "-");
+    let arr = num.split(/[eE]/);
     if (arr.length < 2) return sign + num;
-    let dot = (.1).toLocaleString().substr(1, 1), n = arr[0], exp = +arr[1],
-        w = (n = n.replace(/^0+/, '')).replace(dot, ''),
-      pos = n.split(dot)[1] ? n.indexOf(dot) + exp : w.length + exp,
-      L = pos - w.length, s = "" + BigNumber.from(w);
-      w = exp >= 0 ? (L >= 0 ? s + "0".repeat(L) : r()) : (pos <= 0 ? "0" + dot + "0".repeat(Math.abs(pos)) + s : r());
-    L= w.split(dot); if (L[0]==0 && L[1]==0 || (+w==0 && +s==0) ) w = 0; //** added 9/10/2021
-    return sign + w;
-    function r() {return w.replace(new RegExp(`^(.{${pos}})(.)`), `$1${dot}$2`)}
-  }
+    let n = arr[0];
+    let exp = +arr[1];
+    let dot = '.';
+    let parts = n.split(dot);
+    let integerPart = parts[0];
+    let fractionalPart = parts[1] || "";
+    if (exp > 0) {
+        if (fractionalPart.length <= exp) {
+            fractionalPart = fractionalPart.padEnd(exp, '0');
+            n = integerPart + fractionalPart;
+        } else {
+            n = integerPart + fractionalPart.substring(0, exp) + dot + fractionalPart.substring(exp);
+        }
+    } else {
+        if (integerPart.length + exp > 0) {
+            n = integerPart.slice(0, exp) + dot + integerPart.slice(exp) + fractionalPart;
+        } else {
+            n = "0" + dot + "0".repeat(Math.abs(exp) - integerPart.length) + integerPart + fractionalPart;
+        }
+    }
+    n = n.replace(/^0+(?=\d)|(\.0+|(?<=\.\d*)0+)$/g, '');
+    return sign + n;
+};
+
 
 export const isUrlValid = (url) => {
     try {
