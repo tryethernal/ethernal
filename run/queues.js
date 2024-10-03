1a7b8c421fd2a543fc737e7188e502de2225172a
Antoine de Chevigné <antoine@tryethernal.com>
2024-10-03 12:23:14 +0200

    adjust hpworker settings

--- run/queues.js
+++ run/queues.js
@@ -6,17 +6,19 @@ const queues = {};
 priorities['high'].forEach(jobName => {
     queues[jobName] = new Queue(jobName, {
         defaultJobOptions: {
-            attempts: 50,
+            attempts: 5,
             removeOnComplete: {
                 count: 100,
                 age: 4 * 60
             },
             timeout: 30000,
             backoff: {
-                type: 'exponential',
-                delay: 1000
-            }
+                type: 'fixed',
+                delay: 30000
+            },
         },
+        stalledInterval: 29000,
+        maxStalledCount: 5,
         connection,
     });
 });
