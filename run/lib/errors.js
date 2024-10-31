0ccf0c67116f2af7a65d697e6ba00d039b747586
Antoine de Chevigné <antoine@tryethernal.com>
2024-10-31 21:16:14 +0100

    do not send to sentry handled erros

--- run/lib/errors.js
+++ run/lib/errors.js
@@ -4,15 +4,6 @@ const logger = require('./logger');
 const managedError = (error, req, res, status_code = 400, capture = true) => {
     logger.error(error.message, error, { ...req.params, ...req.query });
 
-    if (capture && !error.message.startsWith('Timed out after')) {
-        Sentry.setContext('params', { ...req.params, ...req.query });
-        res.sentry = Sentry.captureException(error, {
-            tags: {
-                route: req.baseUrl + req.route.path,
-                status_code
-            }
-        });
-    }
     return res.status(status_code).send(error.message);
 };
 
