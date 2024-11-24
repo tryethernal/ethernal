2caa065be631c179aba3a973a79776a854e3377f
Antoine de Chevigné <antoine@tryethernal.com>
2024-11-24 13:03:23 +0700

    fix job

--- run/jobs/deleteDuplicateBalanceChangeForWorkspace.js
+++ run/jobs/deleteDuplicateBalanceChangeForWorkspace.js
@@ -1,31 +1,23 @@
-const { Workspace, TokenBalanceChange } = require('../models');
+const { TokenBalanceChange } = require('../models');
 
 module.exports = async job => {
     const data = job.data;
-    
-    if (!data.workspaceId)
+
+    if (!data.transactionId)
         return 'Missing parameter';
 
-    const workspace = await Workspace.findByPk(data.workspaceId);
-    if (!workspace)
-        return 'Workspace not found';
+    const tokenBalanceChanges = await TokenBalanceChange.findAll({ where: { transactionId: data.transactionId }});
+    const groupedBalanceChanges = {};
+    for (const tokenBalanceChange of tokenBalanceChanges) {
+        if (!groupedBalanceChanges[`${tokenBalanceChange.address}-${tokenBalanceChange.token}`])
+            groupedBalanceChanges[`${tokenBalanceChange.address}-${tokenBalanceChange.token}`] = [];
 
-    const [transactions] = await workspace.getTransactionsWithDuplicateTokenBalanceChanges();
+        groupedBalanceChanges[`${tokenBalanceChange.address}-${tokenBalanceChange.token}`].push(tokenBalanceChange);
+    }
 
-    for (const transaction of transactions) {
-        const tokenBalanceChanges = await TokenBalanceChange.findAll({ where: { transactionId: transaction.transactionId }});
-        const groupedBalanceChanges = {};
-        for (const tokenBalanceChange of tokenBalanceChanges) {
-            if (!groupedBalanceChanges[`${tokenBalanceChange.address}-${tokenBalanceChange.token}`])
-                groupedBalanceChanges[`${tokenBalanceChange.address}-${tokenBalanceChange.token}`] = [];
-
-            groupedBalanceChanges[`${tokenBalanceChange.address}-${tokenBalanceChange.token}`].push(tokenBalanceChange);
-        }
-
-        for (const balanceChanges of Object.values(groupedBalanceChanges)) {
-            const duplicates = balanceChanges.slice(1);
-            for (const duplicate of duplicates)
-                await duplicate.safeDestroy();
-        }
+    for (const balanceChanges of Object.values(groupedBalanceChanges)) {
+        const duplicates = balanceChanges.slice(1);
+        for (const duplicate of duplicates)
+            await duplicate.safeDestroy();
     }
 };
