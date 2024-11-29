123e5a477420b61b73b1aa7444b87c3f819acc4c
Antoine de Chevigné <antoine@tryethernal.com>
2024-11-28 23:20:26 -0800

    reduce monitoring interval

--- run/scheduler.js
+++ run/scheduler.js
@@ -3,7 +3,7 @@ const { enqueue } = require('./lib/queue');
 const INTEGRITY_CHECK_INTERVAL = 5 * 60 * 1000;
 const RPC_HEALTH_CHECK_INTERVAL = 5 * 60 * 1000;
 const SUBSCRIPTION_CHECK_INTERVAL = 5 * 60 * 1000;
-const QUEUE_MONITORING_INTERVAL = 10 * 1000;
+const QUEUE_MONITORING_INTERVAL = 60 * 1000;
 const CANCEL_DEMO_INTERVAL = 60 * 60 * 1000;
 
 (async () => {
