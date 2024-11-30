1b8336b37a47deca966718fce994c0fff3033ae3
Antoine <adechevigne@gmail.com>
2024-11-30 06:35:19 +0100

    integrity check v2 (#394)
    Co-authored-by: Antoine de Chevigné <antoine@tryethernal.com>

--- run/jobs/integrityCheck.js
+++ run/jobs/integrityCheck.js
@@ -1,18 +1,4 @@
-/*
-    This job checks the integrity of a workspace, by using the following.
-    Here, "islands" refers to the islands & gap algorithm (See for example: https://medium.com/trendyol-tech/using-sqls-classical-islands-and-gaps-approach-for-data-quality-ddb05e27faa9)
-    - Fetch latest block checked and check islands between (latest checked || integrity start block) and latest stored
-    - If latest checked == latest stored, fetch the latest block from the chain
-        - If same than latest stored, then OK do nothing
-        - If different & mined less than DELAY_BEFORE_RECOVERY after latest checked, then OK do nothing (we assume a bit of latency for processing)
-        - If different & mined more than DELAY_BEFORE_RECOVERY after latest checked, enqueue missing block for processing
-    - If latest check != latest stored, update latest checked to latest stored
-
-*/
-
-const { Op } = require('sequelize');
 const models = require('../models');
-const db = require('../lib/firebase');
 const { enqueue, bulkEnqueue } = require('../lib/queue');
 const { withTimeout } = require('../lib/utils');
 const moment = require('moment');
@@ -20,7 +6,6 @@ const moment = require('moment');
 const Workspace = models.Workspace;
 
 const DELAY_BEFORE_RECOVERY = 2 * 60;
-const PARTIAL_BLOCK_TTL = 15 * 60;
 
 module.exports = async job => {
     const data = job.data;
@@ -48,6 +33,9 @@ module.exports = async job => {
     if (!workspace.public)
         return 'Not allowed on private workspaces';
 
+    if (workspace.skipIntegrityCheck)
+        return 'Integrity check disabled';
+
     if (!workspace.explorer)
         return 'Should have an explorer associated';
 
@@ -68,59 +56,22 @@ module.exports = async job => {
         Mostly to avoid starting in recovery mode. Also, maybe there is a reason the
         sync hasn't been intiated yet.
     */
-    const existingBlocks = await workspace.getBlocks({ limit: 1 });
-    if (!existingBlocks.length)
-        return 'No block synced yet';
-
     const [lowestBlock] = await workspace.getBlocks({
-        where: {
-            state: 'ready'
-        },
         order: [['number', 'ASC']],
         limit: 1
     });
+    if (!lowestBlock)
+        return 'No block synced yet';
 
-    let lowerBlock;
     /*
-        If we are just starting to check integrity, or if the start block is below/above
-        the latest checked one (if we've changed it for example), we start from the
-        starting block defined on the workspace.
+        If the first block does not exist, we sync it & exit, and the next run
+        will be able to start integrity checks from it.
     */
-    if (
-        !workspace.integrityCheck
-        || workspace.integrityCheckStartBlockNumber < lowestBlock.number
-        || (workspace.integrityCheck.block && workspace.integrityCheckStartBlockNumber > workspace.integrityCheck.block.number)
-    ) {
-        ([lowerBlock] = await workspace.getBlocks({
-            where: {
-                state: 'ready',
-                number: workspace.integrityCheckStartBlockNumber
-            },
-        }));
-
-        /*
-            If the first block does not exist, we sync it & exit, and the next run
-            will be able to start integrity checks from it.
-        */
-        if (!lowerBlock) {
-            await workspace.safeDeleteIntegrityCheck();
-            return await enqueue(`blockSync`, `blockSync-${workspace.id}-${workspace.integrityCheckStartBlockNumber}`, {
-                userId: workspace.user.firebaseUserId,
-                workspace: workspace.name,
-                blockNumber: workspace.integrityCheckStartBlockNumber,
-                source: 'integrityCheck'
-            }, 1);
-        }
-
-        await db.updateWorkspaceIntegrityCheck(workspace.id, { blockId: lowerBlock.id });
-    }
-    else {
-        lowerBlock = workspace.integrityCheck.block;
-    }
-
+    const [lowerBlock] = await workspace.getBlocks({
+        where: { number: workspace.integrityCheckStartBlockNumber }
+    });
     if (!lowerBlock) {
-        await workspace.safeDeleteIntegrityCheck();
-        return await enqueue(`blockSync`, `blockSync-${workspace.id}-${workspace.integrityCheckStartBlockNumber}`, {
+        return enqueue(`blockSync`, `blockSync-${workspace.id}-${workspace.integrityCheckStartBlockNumber}`, {
             userId: workspace.user.firebaseUserId,
             workspace: workspace.name,
             blockNumber: workspace.integrityCheckStartBlockNumber,
@@ -128,49 +79,35 @@ module.exports = async job => {
         }, 1);
     }
 
-    const [upperBlock] = await workspace.getBlocks({
-        where: { state: 'ready' },
-        order: [['number', 'DESC']],
-        limit: 1
-    });
+    const latestReadyBlock = await workspace.getLatestReadyBlock();
+    if (!latestReadyBlock || !latestReadyBlock.timestamp || !latestReadyBlock.number)
+        return 'Invalid latest ready block';
 
-    if (!upperBlock)
-        return 'Missing upper block';
-
-    if (lowerBlock.number == upperBlock.number) {
-        const provider = workspace.getProvider();
-        let latestBlock;
-        try {
-            latestBlock = await withTimeout(provider.fetchLatestBlock());
-        } catch(_error) {
-            return "Couldn't reach network";
-        }
-        /*
-            If the latest block stored is more than 2 minutes away from the latest block on chain,
-            we recover the range of missing blocks
-        */
-        const diff = moment.unix(latestBlock.timestamp).diff(moment(upperBlock.timestamp), 'seconds');
-        if (diff > DELAY_BEFORE_RECOVERY) {
-            await enqueue('batchBlockSync', `batchBlockSync-${workspace.id}-${upperBlock.number}-${latestBlock.number}`, {
-                userId: workspace.user.firebaseUserId,
-                workspace: workspace.name,
-                from: upperBlock.number,
-                to: latestBlock.number,
-                source: 'recovery'
-            });
-        }
+    const provider = workspace.getProvider();
+    let latestBlock;
+    try {
+        latestBlock = await withTimeout(provider.fetchLatestBlock());
+    } catch (_error) {
+        return "Couldn't reach network";
     }
-    const gaps = await workspace.findBlockGaps(lowerBlock.number, upperBlock.number);
-
     /*
-        If we just handled a gap, We can't update latest block checked because sync is done asynchronously.
-        So we just wait for the first run with no gaps.
+        If the latest block stored is more than 2 minutes away from the latest block on chain,
+        we recover the range of missing blocks
     */
-    if (!gaps.length) {
-        if (lowerBlock.number != upperBlock.number)
-            await db.updateWorkspaceIntegrityCheck(workspace.id, { blockId: upperBlock.id });
+    const diff = moment.unix(latestBlock.timestamp).diff(moment(latestReadyBlock.timestamp), 'seconds');
+    if (diff > DELAY_BEFORE_RECOVERY) {
+        await enqueue('batchBlockSync', `batchBlockSync-${workspace.id}-${latestReadyBlock.number}-${latestBlock.number}`, {
+            userId: workspace.user.firebaseUserId,
+            workspace: workspace.name,
+            from: latestReadyBlock.number,
+            to: latestBlock.number,
+            source: 'recovery'
+        });
     }
-    else {
+
+    const gaps = await workspace.findBlockGapsV2(lowerBlock.number, latestBlock.number);
+
+    if (gaps.length) {
         const batches = [];
         for (let j = 0; j < gaps.length; j++) {
             const gap = gaps[j];
