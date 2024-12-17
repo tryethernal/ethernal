066da5b4d6989188d02fdc3e0a50958461d69b2e
Antoine de Chevigné <antoine@tryethernal.com>
2024-12-17 22:13:00 +0100

    improve deletion

--- run/models/tokenbalancechangeevent.js
+++ run/models/tokenbalancechangeevent.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      TokenBalanceChangeEvent.belongsTo(models.TokenBalanceChange, { foreignKey: 'tokenBalanceChangeId', as: 'tokenBalanceChange' });
     }
   }
   TokenBalanceChangeEvent.init({
