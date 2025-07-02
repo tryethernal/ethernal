e67d032f5a7ecaac816893bd3591ca0f9d220977
Antoine de Chevigné <antoine@tryethernal.com>
2025-07-02 20:06:31 -0300

    fix demo migration

--- run/api/demo.js
+++ run/api/demo.js
@@ -114,7 +114,7 @@ router.post('/migrateExplorer', authMiddleware, async (req, res, next) => {
 
         const subscription = await stripe.subscriptions.create({
             customer: user.stripeCustomerId,
-            items: [{ price: getDemoTrialSlug() }],
+            items: [{ price: plan.stripePriceId }],
             trial_period_days: getDefaultExplorerTrialDays(),
             trial_settings: {
                 end_behavior: { missing_payment_method: 'cancel' }
