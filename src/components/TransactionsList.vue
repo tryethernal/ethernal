7f34dbc0715595d1b8f59014935be0291e7cb41b
Antoine de Chevigné <antoine@tryethernal.com>
2025-07-28 12:40:56 +0200

    better contract display

--- src/components/TransactionsList.vue
+++ src/components/TransactionsList.vue
@@ -65,7 +65,7 @@
         <template v-slot:item.to="{ item }">
             <v-chip size="x-small" class="mr-2" v-if="item.to && item.to === currentAddress">self</v-chip>
             <template v-if="!item.to && item.receipt?.contractAddress">Created: </template>
-            <Hash-Link :type="'address'" :hash="item.to || item.receipt?.contractAddress" :withTokenName="false" :withName="true" :contract="item.contract ||item.receipt?.createdContract" :key="item.hash"/>
+            <Hash-Link :type="'address'" :hash="item.to || item.receipt?.contractAddress" :withTokenName="false" :withName="true" :contract="item.contract || item.receipt?.createdContract" :key="item.hash"/>
         </template>
         <template v-slot:item.value="{ item }">
             {{ $fromWei(item.value, 'ether', currentWorkspaceStore.chain.token, false, 4) }}
