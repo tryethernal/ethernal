7f72d0f6b4c8b9bb2aac144d1901e3e539caf8a1
Antoine de Chevigné <antoine@tryethernal.com>
2025-08-13 10:26:57 +0200

    switch to mailgun

--- run/tests/jobs/sendResetPasswordEmail.test.js
+++ run/tests/jobs/sendResetPasswordEmail.test.js
@@ -1,9 +1,8 @@
 require('../mocks/lib/crypto');
 require('../mocks/lib/flags');
-require('../mocks/lib/sendgrid');
+require('../mocks/lib/mailgun');
 
 const flags = require('../../lib/flags');
-const sgMail = require('@sendgrid/mail');
 
 const sendResetPasswordEmail = require('../../jobs/sendResetPasswordEmail');
 
@@ -12,17 +11,17 @@ beforeEach(() => jest.clearAllMocks());
 describe('batchBlockSync', () => {
     it('Should send a reset password email', (done) => {
         sendResetPasswordEmail({ data: { email: 'antoine@tryethernal.com' }})
-            .then(() => {
-                expect(sgMail.send).toHaveBeenCalled();
+            .then(res => {
+                expect(res).toEqual([{ statusCode: 202 }]);
                 done();
             });
     });
 
-    it('Should raise an error if Sendgrid is not enabled', (done) => {
-        jest.spyOn(flags, 'isSendgridEnabled').mockReturnValueOnce(false);
+    it('Should raise an error if Mailgun is not enabled', (done) => {
+        jest.spyOn(flags, 'isMailgunEnabled').mockReturnValueOnce(false);
         sendResetPasswordEmail({ data: { email: 'antoine@tryethernal.com' }})
             .catch(error => {
-                expect(error.message).toEqual('Sendgrid has not been enabled.');
+                expect(error.message).toEqual('Mailgun has not been enabled.');
                 done();
             });
     });
