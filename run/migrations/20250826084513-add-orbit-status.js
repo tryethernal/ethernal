98ed89f0782b6d0ec054b7e162f1e88b0f2671b3
Antoine de Chevigné <antoine@tryethernal.com>
2025-09-08 15:25:19 +0200

    fix migrations

--- run/migrations/20250826084513-add-orbit-status.js
+++ run/migrations/20250826084513-add-orbit-status.js
@@ -5,8 +5,6 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     const transaction = await queryInterface.sequelize.transaction();
     try {
-      await queryInterface.removeColumn('orbit_nodes', 'confirmed', { transaction });
-      await queryInterface.removeColumn('orbit_nodes', 'rejected', { transaction });
       await queryInterface.addColumn('orbit_nodes', 'status', {
         type: Sequelize.ENUM('pending', 'confirmed', 'rejected'),
         allowNull: true,
@@ -23,16 +21,6 @@ module.exports = {
     const transaction = await queryInterface.sequelize.transaction();
     try {
       await queryInterface.removeColumn('orbit_nodes', 'status', { transaction });
-      await queryInterface.addColumn('orbit_nodes', 'confirmed', {
-        type: Sequelize.BOOLEAN,
-        allowNull: true,
-        defaultValue: false
-      }, { transaction });
-      await queryInterface.addColumn('orbit_nodes', 'rejected', {
-        type: Sequelize.BOOLEAN,
-        allowNull: true,
-        defaultValue: false
-      }, { transaction });
       await transaction.commit();
     } catch (error) {
       await transaction.rollback();
